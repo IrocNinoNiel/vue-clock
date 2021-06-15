9cab00800e458acfad1bed7b634100d2d0c76ea1
Iroc <iroc.niel123@gmail.com>
2021-06-15 16:22:30 +0800

    'Add timer functionality

--- src/views/timer/Timer.vue
+++ src/views/timer/Timer.vue
@@ -1,15 +1,59 @@
 <template>
     <div>
         <h1>Timer</h1>
+        <br/>
+        <VueTimer :time-left="timeLeft" :timeLimit="timeLimit"/> 
+        <br/>
+        <button class="button" :style="{background:'#4CAF50'}" @click="startTimer" v-show="!started">Start</button>
+        <button class="button" :style="{background:'#f44336'}" @click="stopTimer" v-show="started">Stop</button>
     </div>
 </template>
 
 <script>
+    import VueTimer from '../../components/Vuetimer.vue'
     export default {
-        name:'Timer'
+        name:'Timer',
+        components:{
+            VueTimer,
+        },
+        data() {
+            return {
+                timeLimit: 20,
+                timePassed: 0,
+                timerInterval: null,
+                started: false,
+            }
+        },
+        computed: {
+            timeLeft() {
+                return this.timeLimit - this.timePassed
+            },
+        },
+        methods: {
+            startTimer() {
+                this.timerInterval = setInterval(() => (this.timePassed += 1), 1000);
+                this.started = true;
+            },
+            stopTimer() {
+                clearInterval(this.timerInterval);
+                this.started = false;
+            }
+            
+        },
     }
 </script>
 
 <style scoped>
+
+.button {
+    /* background-color: #4CAF50; Green */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+}
 
 </style>
